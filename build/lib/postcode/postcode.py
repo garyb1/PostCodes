89ab73b7fed1e1409929e9adb53d9878298572d1
Gary Byrne <gary.byrne39@mail.dcu.ie>
2017-03-30 13:09:50 +0100

    first commit

---
+++ build/lib/postcode/postcode.py
@@ -0,0 +1,13 @@
+"""
+
+Write a library that supports validating and formatting post codes for UK.
+The details of which post codes are valid and which are the parts they consist of can be
+found at https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom#Formatting.
+The API that this library provides is your choice.
+
+Author:  Gary Byrne
+Created: 29/03/2017
+
+"""
+
+from lib import PostCodeClient
---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='Postcodes',
+      version='1.0',
+      py_modules=['postcode/postcode'],
+      )
